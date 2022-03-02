a53be58e070340fad9517e81988ba775b20eed7a
jotaluna6 <jotaluna6@users.noreply.github.com>
2022-03-02 01:21:37 +0000

    deploy: 9020771e8c99fb3aed27eed4a321662ea9d32454

--- _dist_/index.js
+++ _dist_/index.js
@@ -1,6 +1,50 @@
-/**
- * This file is just a silly example to show everything working in the browser.
- * When you're ready to start on your site, clear the file. Happy hacking!
- **/
+ 
+import { registerImage } from './lazy.js'
 
-console.log('Happy hacking :)')
+// crear imagen
+const maximo= 123;
+const minimo= 1;
+
+const random= ()=> Math.floor(Math.random() * (maximo - minimo) + minimo);
+const createImageNode= () => {
+
+  const container= document.createElement('div');
+  container.className='p-4';
+
+  const imagen= document.createElement('img')
+  imagen.className='mx-auto';
+  imagen.width='320';
+  imagen.dataset.src=`https://randomfox.ca/images/${random()}.jpg`;
+  const fondo = document.createElement("div");
+  fondo.className = "bg-gray-300";
+  fondo.style.minHeight = "100px";
+  fondo.style.display = "inline-block";
+  fondo.appendChild(imagen);
+  container.appendChild(fondo);
+  agregadas++;
+  infoq();
+  return container;
+}
+const mountNode= document.getElementById('images')
+const addButton=  document.querySelector('#add');
+const clearButton=  document.querySelector('#remove');
+
+const addImage= () => {
+    const newImage= createImageNode();
+    mountNode.append(newImage) ;
+    registerImage   (newImage);
+
+}
+const clearImage= () => {
+  
+    while (mountNode.hasChildNodes()) {
+        mountNode.removeChild(mountNode.firstChild);
+}
+agregadas=0;
+cargadas=0;
+}
+addButton.addEventListener('click', addImage);
+clearButton.addEventListener('click', clearImage);
+/* const nuevaImagen= createImageNode();
+ */
+
